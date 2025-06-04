2363d9dc20db73f2b94cd752f7d38b6e8dc2a9ee
Rudra Pratap Padhi <rudrapratappadhi2003@gmail.com>
2025-06-05 00:13:54 +0530

    Responsive check done

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -7,27 +6,26 @@ import Skills from './pages/Skills'
 import ContactMe from './pages/ContactMe'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="relative min-h-screen overflow-x-hidden">
-      <div
-        className="fixed top-0 left-0 w-full h-full -z-10"
-        style={{
-          background: 'linear-gradient(135deg, #0b0c0f, #1a1c20)',
-          backgroundAttachment: 'fixed',
-          backgroundSize: 'cover',
-        }}
-      />
-      <Home />
-      <About />
-      <Skills/>
-      <ProjectSection/>
-      <ContactMe/>
-    </div>
+      <div className="relative overflow-x-hidden">
+        <div
+          className="fixed top-0 left-0 w-full h-full -z-10"
+          style={{
+            background: 'linear-gradient(135deg, #0b0c0f, #1a1c20)',
+            backgroundAttachment: 'fixed',
+            backgroundSize: 'cover',
+          }}
+        />
+        <Home />
+        <About />
+        <Skills />
+        <ProjectSection />
+        <ContactMe />
+      </div>
     </>
-  )
+  );
 }
+
 
 export default App
